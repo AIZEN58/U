5c1ec3c578f7ae8b7621bb844286aae82aaf3e9e
AIZEN58 <162022243+AIZEN58@users.noreply.github.com>
2024-03-18 06:47:50 +0300

    Update شكل.js

--- plugins/شكل.js
+++ plugins/شكل.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
   if (!text) throw `*This command generates images from text prompts*\n\n*مثال usage*\n*◉ ${usedPrefix + command} فتاة انمي جميلة*\n*◉ ${usedPrefix + command} ايلون ماسك بالزي الوردي*`;
 
   try {
-    m.reply('* من فضلك انتظر, يتم العمل على انشاء الصورة   *');
+    m.reply('*من فضلك انتظر, يتم العمل على انشاء الصورة*');
 
     const endpoint = `https://cute-tan-gorilla-yoke.cyclic.app/imagine?text=${encodeURIComponent(text)}`;
     const response = await fetch(endpoint);
