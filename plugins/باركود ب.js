7966aab5733087c2cff929d3760ba7c515df27b4
AIZEN58 <162022243+AIZEN58@users.noreply.github.com>
2024-03-20 06:16:43 +0300

    Update and rename 2باركود.js to باركود ب.js

--- plugins/باركود ب.js
+++ plugins/باركود ب.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch'
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 let q = m.quoted ? m.quoted : m
 let mime = (q.msg || q).mimetype || ''
-if (!mime) throw '*امن فضلك رد بالباركود اللذي تريد تحويله الى رمز*'
+if (!mime) throw '*من فضلك رد بالباركود الذي تريد تحويله الى رمز*'
 let img = await q.download?.()
 let url = await uploadImage(img)
 let anu = await fetch(`https://api.lolhuman.xyz/api/read-qr?apikey=${lolkeysapi}&img=${url}`)
